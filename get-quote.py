df59b63eef7596461a53d8afbe05ffaa6f095c7c
Redgatorade000 <red@gatorade.com>
2021-04-24 17:57:59 +0200

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -5,9 +5,9 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
+  last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  print(quotes[last],quotes[rnd],end = ' ')
 
 if __name__== "__main__":
   primary()
